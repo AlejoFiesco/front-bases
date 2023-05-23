f68ed071243166523d089dfdb545aff5771cdf69
Katherine856 <kathyuska97@gmail.com>
2023-05-23 00:57:39 -0500

    login listo

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
-import { AppComponent } from './app.component';
+import { PrincipalComponent } from './principal/principal.component';
 
 const routes: Routes = [
-  {path: '', component: AppComponent},
+  {path: '', component: PrincipalComponent},
   {path: 'login', component: LoginComponent}
 ];
 
